0253da487eb58a4fba80810a548e773391b3244e
Abu Huraira Khan <abuhurairakhan3127@gmail.com>
2024-12-20 21:33:56 +0600

    update_2

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
-const express = require("express");
-const path = require("path");
-const YTDlpWrap = require("yt-dlp-wrap").default;
+const express = require('express')
+const ytdlp = require('ytdlp-nodejs')
+const path = require('path')
+// const url = "https://www.youtube.com/watch?v=Qzc_aX8c8g4";
 
-// Create yt-dlp wrapper instance
-const ytDlpWrap = new YTDlpWrap("./yt-dlp"); // Provide the relative path to the binary
+const app = express()
+const port = 3000
 
-const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
 
-// Serve static files (for the frontend)
-app.use(express.static(path.join(__dirname, "public")));
+app.get('/info', async (req, res) => {// download video
+  const url = req.query.url
+  const getInfo = ytdlp.thumbnail(url);
+  const info = await getInfo;
+  console.log(info)
+  res.json(info)
+})
 
-// Endpoint to fetch video metadata
-app.get("/info", async (req, res) => {
-  const videoUrl = req.query.url;
-  try {
-    let metadata = await ytDlpWrap.getVideoInfo(videoUrl);
-
-    // Find a format with both video and audio
-    const videoWithAudio = metadata.formats.find(
-      (format) => format.acodec !== "none" && format.vcodec !== "none"
-    );
-
-    if (videoWithAudio) {
-      // console.log("Video URL with audio:", videoWithAudio.url);
-      res.json({
-        audio: metadata.formats.filter(
-          (format) =>
-            format.acodec === "opus" && format.format_note.includes("medium")
-        ), // List audio formats
-        video: metadata.formats.filter((format) => format.vcodec !== "none"), // List video formats
-      });
-    } else {
-      res.status(404).json({ error: "No audio-video format found." });
-    }
-  } catch (error) {
-    console.error("Error fetching video metadata:", error);
-    res.status(500).json({ error: "Error fetching video metadata" });
-  }
-});
-
-// Endpoint to download audio
-
-// Start the server
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
